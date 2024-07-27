e2d29cd445183bda2ac53302581527fd405261e8
Piama <athelec@gmail.com>
2024-07-27 04:00:39 +0200

    Correction rate

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,13 +1,34 @@
 import React from "react";
-
+import fullStar from "../assets/fullStar.png"
+import emptyStar from "../assets/emptyStar.png"
 
 // Récupération des tags
 const Rate = ({rating}) => {
-    console.log(rating);
-    const score = rating.score;
+    const score = rating;
     const notes=[1, 2, 3, 4, 5]
+    console.log(score);
     return (
-        <div>dzed</div>
+        <div className="rates-section">
+            <div>
+            {notes.map((note) => (
+                //Si mon score supp ou égal à la note alors
+                        score >= note ? (
+                            <img className="star" alt="full-star"
+                            //To string to convert data en en chaine de caractères
+                            key ={note.toString()}
+                            src={fullStar}
+                            
+                            />
+                        ) : ( // Ou..
+                            <img className="star" alt="full-star"
+                            key ={note.toString()}
+                            src={emptyStar}
+                            />
+                        )
+                        ))}
+
+            </div>
+        </div>
     )
 }
 
